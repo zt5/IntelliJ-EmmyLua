203b2bf9907af9d05fd8f9667c7cc5859456c007
zhutie <zhu.tie@163.com>
2020-07-27 17:42:22 +0800

    recover old debugger

--- src/main/java/com/tang/intellij/lua/debugger/app/LuaAppRunConfiguration.kt
+++ src/main/java/com/tang/intellij/lua/debugger/app/LuaAppRunConfiguration.kt
@@ -151,9 +151,6 @@ class LuaAppRunConfiguration(project: Project, factory: ConfigurationFactory)
 
     override fun checkConfiguration() {
         super.checkConfiguration()
-        if (debuggerType == DebuggerType.Attach) {
-            throw RuntimeConfigurationError("Unfortunately, attach debugger is no longer available. Please try remote debugger.")
-        }
 
         val program = program
         if (program.isEmpty()) {
--- src/main/java/com/tang/intellij/lua/debugger/app/LuaAppSettingsEditor.java
+++ src/main/java/com/tang/intellij/lua/debugger/app/LuaAppSettingsEditor.java
@@ -27,6 +27,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.ui.HoverHyperlinkLabel;
 import com.intellij.ui.HyperlinkAdapter;
 import com.intellij.ui.RawCommandLineEditor;
@@ -69,16 +70,19 @@ public class LuaAppSettingsEditor extends SettingsEditor<LuaAppRunConfiguration>
         descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
         myWorkingDir.addBrowseFolderListener("Choose Working Dir", "Choose working dir", project, descriptor);
 
-        DebuggerType[] debuggerTypes = new DebuggerType[] { DebuggerType.Mob };
-        /*if (SystemInfoRt.isWindows)
+        DebuggerType[] debuggerTypes;
+        if (SystemInfoRt.isWindows)
             debuggerTypes = new DebuggerType[] { DebuggerType.Attach, DebuggerType.Mob };
         else
-            debuggerTypes = new DebuggerType[] { DebuggerType.Mob };*/
+            debuggerTypes = new DebuggerType[] { DebuggerType.Mob };
 
         DefaultComboBoxModel<DebuggerType> debuggerDataModel = new DefaultComboBoxModel<>(debuggerTypes);
         myDebugger.setModel(debuggerDataModel);
         myDebugger.addItemListener(e -> {
-            onDebuggerTypeChanged();
+            DebuggerType debuggerType = (DebuggerType) myDebugger.getSelectedItem();
+            mobdebugLink.setVisible(debuggerType == DebuggerType.Mob);
+            showConsoleWindowCheckBox.setVisible(debuggerType == DebuggerType.Attach);
+
             fireEditorStateChanged();
         });
 
@@ -87,14 +91,6 @@ public class LuaAppSettingsEditor extends SettingsEditor<LuaAppRunConfiguration>
         DefaultComboBoxModel<String> outputCharsetModel = new DefaultComboBoxModel<>(ArrayUtil.toStringArray(charsetSortedMap.keySet()));
         outputCharset.setModel(outputCharsetModel);
         outputCharset.addItemListener(e -> fireEditorStateChanged());
-
-        onDebuggerTypeChanged();
-    }
-
-    private void onDebuggerTypeChanged() {
-        DebuggerType debuggerType = (DebuggerType) myDebugger.getSelectedItem();
-        mobdebugLink.setVisible(debuggerType == DebuggerType.Mob);
-        showConsoleWindowCheckBox.setVisible(debuggerType == DebuggerType.Attach);
     }
 
     @Override
